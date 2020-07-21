08ea3eb5f9a9df478162eb2482080634bd9def3e
hex@telios.io <hex@telios.io>
2020-07-21 09:00:15 -0400

    Cores now match in test

--- tests/hypercore.test.js
+++ tests/hypercore.test.js
@@ -13,7 +13,7 @@ test('Hypercore - create and get core', async t => {
 
   t.ok(key, `Generated core key 1`);
 
-  const core2 = await Hypercore.getCore('c20ebbfc5702bd4aabf86e055463c011bdcfd24785039c7d70d2be5e6016c7b5');
+  const core2 = await Hypercore.getCore(key);
   const key2 = core2.key.toString('hex');
   await core2.close();
 
