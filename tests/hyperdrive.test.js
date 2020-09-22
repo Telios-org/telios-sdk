5040dc737b2217866eff2f19ca4f49084f3ba924
hex@telios.io <hex@telios.io>
2020-09-21 23:41:14 -0400

    Adding method for clearing files from drives

--- tests/hyperdrive.test.js
+++ tests/hyperdrive.test.js
@@ -1,32 +1,33 @@
 const conf = require('./conf');
+const fs = require('fs');
 const tape = require('tape');
 const _test = require('tape-promise').default;
 const test = _test(tape);
 const { Hyperdrive } = require('..');
 const SDK = require('dat-sdk');
+let hyperdrive = null;
 
 test('Hyperdrive - create drive', async t => {
   t.plan(1);
 
   try {
-    const sdk = await SDK();
+    const sdk = await SDK({
+      storage: __dirname + '/storage'
+    });
 
     const opts = {
-      name: 'Test Drive',
+      name: 'Test Delete Drive',
       sdk: sdk,
       driveOpts: {
         persist: false
       }
     };
 
-    const hyperdrive = new Hyperdrive(opts);
+    hyperdrive = new Hyperdrive(opts);
     await hyperdrive.connect();
     const drive = hyperdrive.drive;
 
     const key = drive.key.toString('hex');
-
-    await drive.destroyStorage();
-    await hyperdrive.close();
 
     t.ok(key, `Generated Hyperdrive key ${key}`);
   } catch (err) {
@@ -34,4 +35,33 @@ test('Hyperdrive - create drive', async t => {
   }
 });
 
-test.onFinish(() => process.exit(0));
+test('Hyperdrive - remove file', async t => {
+  t.plan(1);
+
+  try {
+    const readStream = fs.createReadStream(__dirname + '/data/raw.email');
+    const writeStream = hyperdrive.drive.createWriteStream('emails/raw.email');
+    
+    readStream.pipe(writeStream);
+
+    // TODO: Update this test to confirm data has been freed up
+    hyperdrive.drive.on('update', async () => {
+      try {
+        await hyperdrive.removeFile('emails/raw.email');
+        t.ok(true);
+      } catch (err) {
+        // no catch
+      }
+    });
+
+  } catch (err) {
+    t.error(err);
+  }
+});
+
+test.onFinish(async () => {
+  await hyperdrive.close();
+  await hyperdrive.drive.destroyStorage();
+  
+  process.exit(0);
+});
