5e5f1fc050c8790981637b8a2b758ce76f6fefe2
hexadecible <63071090+hexadecible@users.noreply.github.com>
2021-03-09 23:27:55 -0500

    Bug fixes

--- tests/drive.transact.test.js
+++ tests/drive.transact.test.js
@@ -21,27 +21,15 @@ const drive3Path = path.join(__dirname, '/drive_clone3');
 const { secretBoxKeypair: keyPair2 } = Account.makeKeys();
 const { secretBoxKeypair: keyPair3 } = Account.makeKeys();
 
-const drive1 = new Drive(drive1Path, null, {
-  keyPair,
-  live: true,
-  watch: true,
-  seed: true
-});
+const drive1 = new Drive(drive1Path, null, { keyPair });
 
-const drive2 = new Drive(drive2Path, drivePubKey, {
-  keyPair: keyPair2,
-  live: true,
-  watch: false,
-  seed: true,
-  slave: true
-});
+const drive2 = new Drive(drive2Path, drivePubKey, { keyPair: keyPair2, watch: false });
 
 // const drive3 = new Drive(drive3Path, drivePubKey, {
 //   keyPair: keyPair3,
 //   live: true,
 //   watch: false,
-//   seed: true,
-//   slave: true
+//   seed: true
 // });
 
 (async () => {
